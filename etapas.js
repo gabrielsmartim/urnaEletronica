09fede5752e1c567f143f7fa29410fd156cde3a5
unknown <martim.gabriel12@gmail.com>
2021-07-12 22:17:44 -0300

    First Commit

---
+++ etapas.js
@@ -0,0 +1,56 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros:5,
+        candidatos: [
+                {
+                    numero: '38111',
+                    nome:'TIRIRICA',
+                    partido: 'ABC',
+                    fotos: [
+                        {url:'tiririca.jpg', legenda:'VEREADOR', small: false }
+                    ]
+                },
+
+                {
+                    numero: '77222',
+                    nome:'DONALD TRUMP',
+                    partido: 'DEFG',
+                    fotos: [
+                        {url:'DonaldTrump.jpg', legenda:'VEREADOR'}
+                    ]
+                },
+            ]
+    },
+
+    {
+        titulo:'PREFEITO',
+        numeros:2,
+        candidatos: [
+                {
+                    numero: '99',
+                    nome:'Ciclano',
+                    partido: 'ABC',
+                    vice:'Cic',
+                    fotos: [
+                        {url:'Alborghetti.jpg', legenda:'Prefeito'},
+                        {url:'pepe-nenem.jpg', legenda:'Vice-Prefeito', small: true}
+                    ]
+                },
+
+                {
+                    numero: '84',
+                    nome: 'Zulano',
+                    partido:'QWERTY',
+                    vice:'Zul',
+                    fotos: [
+                        {url:'ratinho.jpeg', legenda: 'Prefeito'},
+                        {url:'supla.jpg', legenda: 'vice-prefeito', small: true}
+                    ]
+                },
+            ]
+
+    }
+                    
+              
+]
